6015424ba70d9dcdb1f455959a6dbb12f727dae3
kishna h k <khk803123@gmail.com>
2026-01-16 11:29:05 +0530

    prime no pytest files

---
+++ prime.py
@@ -0,0 +1,20 @@
+def prime_no(num):
+    prime=True
+    if num<2:
+        prime=False
+    else:
+        for i in range(2,num):
+            if num%i==0:
+                prime=False
+
+    return prime
+
+if __name__=="__main__":
+    num=13
+
+    isPrime=prime_no(num)
+
+    if isPrime:
+        print(num, "is a prime no")
+    else:
+        print(num, "is not a prime no")
---
+++ test_prime.py
@@ -0,0 +1,4 @@
+from prime import prime_no
+
+def test_prime_no():
+    assert prime_no(17)==True
